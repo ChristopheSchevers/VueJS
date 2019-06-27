59b024e285271d7dc1c237000bcb2fcc45cf1933
Christophe Schevers <christophe.schevers@gmail.com>
2019-06-27 11:27:33 +0200

    update

--- 3-reactivity/main.js
+++ 3-reactivity/main.js
@@ -1,13 +1,19 @@
-new VTTCue({
+new Vue({
     el: '#app',
     data: {
         counter: 0,
-        result: '',
+        secondCounter: 0,
+    },
+    computed: {
+        output: function(){
+            console.log('Computed');
+            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+        }
     },
     methods: {
-        increase: function(){
-            this.counter++;
-            this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+        result(){
+            console.log('Method');
+            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
         }
     }
 });
